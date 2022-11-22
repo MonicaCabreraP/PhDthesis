cd583d88a3bc87106d1743a3a64126457e2eace5
Mónica Cabrera <31327141+MonicaCabreraP@users.noreply.github.com>
2022-11-22 10:26:29 +0100

    Delete PCHiC directory

--- PCHiC/Step_4_CHiCAGO/merge/CHiCAGO_merge.cmd
+++
@@ -1,4 +0,0 @@
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_merge_NUTWT.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_merge_MOCKWT.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_merge_NUTKO.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_merge_MOCKKO.R
--- PCHiC/Step_4_CHiCAGO/recalibration/CHiCAGO_pipeline_NUTWT_recalibrated.cmd
+++
@@ -1,8 +0,0 @@
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_MOCK1_KO_downsampled.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_MOCK1_WT_downsampled.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_MOCK2_KO_downsampled.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_MOCK2_WT_downsampled.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_NUT1_KO_downsampled.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_NUT1_WT_downsampled.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_NUT2_KO_downsampled.R
-/apps/R/3.6.1/INTEL/bin/Rscript /gpfs/projects/bsc08/bsc08471/p53/Step2_CHiCAGO/downsampled/NUTWT_recalibration/CHiCAGO_pipeline_NUT2_WT_downsampled.R
